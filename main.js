0996d3b2e9e6e281e9d4df600333cf0a189f23bb
shorya <94457442+shoryavardhankhyalia@users.noreply.github.com>
2024-05-30 16:55:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function bed()
+{
+    window.location = "Bed_Room.html";
+}
+function desk()
+{
+    window.location = "desk.html";
+}
+function bottle()
+{
+    window.location = "bottles.html";
+}
+function fruit()
+{
+    window.location = "fruit_basket.html";
+}
+function tv()
+{
+    window.location = "tv.html";
+}
---
+++ tv.js
@@ -0,0 +1,53 @@
+img = "";
+status = "";
+objects = [];
+function preload()
+{
+    img = loadImage("tv.jpg");
+}
+function setup()
+ {
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects";
+ }
+ function modelLoaded()
+ {
+   console.log("ModelLoaded!");
+   status = true;
+   objectDetector.detect(img ,gotResult);
+ }
+ function gotResult(error , results)
+ {
+      if(error)
+         {
+            console.log(error);
+         }
+         else{
+            console.log(results);
+            objects = results;
+         }
+ }
+ function draw()
+ {
+    image(img , 0 , 0 , 640 , 420);
+
+    if (status != "")
+      {
+         for(i = 0; i < objects.length ; i++)
+         {
+             document.getElementById("status").innerHTML = "status : object detected";
+             fill("#FF0000");
+             percent = floor(objects[i].confidence * 100);
+             text(objects[i].label+ " " + percent + "%" , objects[i].x +15 , objects[i].y +15);
+             noFill();
+             stroke("#ff0000");
+             rect(objects[i].x ,objects[i].y ,objects[i].width ,objects[i].height ,);
+         }
+      }
+ }
+ function back()
+ {
+   window.location = "index.html";
+ }
